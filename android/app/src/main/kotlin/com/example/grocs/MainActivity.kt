45c8ba3e89c921b235946c0b0c87084efde9c4ad
Samrat Mukherjee <samratmukherjee.sps@gmail.com>
2021-08-14 08:49:14 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/grocs/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.grocs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColors {
+  static Color lightTheme = Color(0xffff7129);
+  static Color darkTheme = Color(0xff2f2f2b);
+}
